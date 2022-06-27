069eb6b6ab251beb1964292723d7ec80bb4ce724
Davi Pereira Rio <Davi.pereira.rio@gmail.com>
2022-06-27 20:27:03 -0300

    Sidebar Edit

--- app/src/main/java/com/example/maintask/Splash.kt
+++ app/src/main/java/com/example/maintask/Splash.kt
@@ -3,9 +3,12 @@ package com.example.maintask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.graphics.drawable.DrawerArrowDrawable
+import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
@@ -16,6 +19,10 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
+        window.navigationBarColor = ContextCompat.getColor(this@Splash, R.color.black)
+        window.statusBarColor = ContextCompat.getColor(this@Splash, R.color.black)
 
         val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
@@ -28,14 +35,7 @@ class Splash : AppCompatActivity() {
 
         navView.setNavigationItemSelectedListener {
 
-            when(it.itemId){
 
-                R.id.nav_profile -> Toast.makeText(applicationContext,"Perfil", Toast.LENGTH_SHORT).show()
-                R.id.nav_agenda -> Toast.makeText(applicationContext,"Agenda", Toast.LENGTH_SHORT).show()
-                R.id.nav_ajuda -> Toast.makeText(applicationContext,"Ajuda", Toast.LENGTH_SHORT).show()
-                R.id.nav_settings -> Toast.makeText(applicationContext,"Configurações", Toast.LENGTH_SHORT).show()
-
-            }
             true
         }
     }
