79ef0908a23cebc2b6af21e3448d0e34d10363c0
Davi Pereira Rio <Davi.pereira.rio@gmail.com>
2022-06-27 17:37:09 -0300

    SideBar

--- app/src/main/java/com/example/maintask/Splash.kt
+++ app/src/main/java/com/example/maintask/Splash.kt
@@ -2,10 +2,50 @@ package com.example.maintask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.graphics.drawable.DrawerArrowDrawable
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class Splash : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.nav_profile -> Toast.makeText(applicationContext,"Perfil", Toast.LENGTH_SHORT).show()
+                R.id.nav_agenda -> Toast.makeText(applicationContext,"Agenda", Toast.LENGTH_SHORT).show()
+                R.id.nav_ajuda -> Toast.makeText(applicationContext,"Ajuda", Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext,"Configurações", Toast.LENGTH_SHORT).show()
+
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
