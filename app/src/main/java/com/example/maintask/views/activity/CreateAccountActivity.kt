d6360608df880c2c6940d23cbba1efcc334033bf
Eduardo <ceduardolima2000@gmail.com>
2022-06-29 10:50:49 -0300

    Criando a tela de cadastro com o botão de add foto

---
+++ app/src/main/java/com/example/maintask/views/activity/CreateAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.example.maintask.views.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.maintask.R
+
+class CreateAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
