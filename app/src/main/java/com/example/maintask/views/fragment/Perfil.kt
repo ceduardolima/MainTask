d03df8382ab92b43afa739b0959d0f7640cb9eb3
Davi Pereira Rio <Davi.pereira.rio@gmail.com>
2022-07-10 21:28:36 -0300

    dLayout fragmnet Perfil

--- app/src/main/java/com/example/maintask/views/fragment/Perfil.kt
+++ app/src/main/java/com/example/maintask/views/fragment/Perfil.kt
@@ -1,12 +1,44 @@
 package com.example.maintask.views.fragment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import com.example.maintask.R
+import com.example.maintask.databinding.FragmentPerfilBinding
+import com.google.android.material.textfield.TextInputLayout
 
-class Perfil : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil)
-    }
-}
+
+class Perfil : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val emailPerfilEditText = view.findViewById<TextInputLayout>(R.id.email_perfil_edit)
+        val nomeEditText = view.findViewById<TextInputLayout>(R.id.nome_perfil_edit)
+        val cargoEditText = view.findViewById<TextInputLayout>(R.id.cargo_perfil_edit)
+        val cidadeEditText = view.findViewById<TextInputLayout>(R.id.cidade_perfil_edit)
+        val salvarButton = view.findViewById<Button>(R.id.btn_perfil_salvar)
+
+        salvarButton.setOnClickListener {
+            salvarButton.isEnabled = false
+            val email = emailPerfilEditText.editText?.text.toString()
+            val password = nomeEditText.editText?.text.toString()
+            val cargo = cargoEditText.editText?.text.toString()
+            val cidade = cidadeEditText.editText?.text.toString()
+
+        }
+
+
+
+
--- app/src/main/java/com/example/maintask/views/fragment/Splash.kt
+++ app/src/main/java/com/example/maintask/views/fragment/Splash.kt
@@ -11,37 +11,12 @@ import com.google.android.material.navigation.NavigationView
 
 class Splash : AppCompatActivity() {
 
-    lateinit var toggle: ActionBarDrawerToggle
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
 
-        window.navigationBarColor = ContextCompat.getColor(this@Splash, R.color.black)
-        window.statusBarColor = ContextCompat.getColor(this@Splash, R.color.black)
-
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
-        val navView : NavigationView = findViewById(R.id.nav_view)
-
-        toggle = ActionBarDrawerToggle(this,drawerLayout, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        navView.setNavigationItemSelectedListener {
-
-            true
-        }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        if(toggle.onOptionsItemSelected(item)){
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
 }
