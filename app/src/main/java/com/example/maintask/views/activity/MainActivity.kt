014af2cba3198abdd60accd7770cd5f3b394adcd
Davi Pereira Rio <Davi.pereira.rio@gmail.com>
2022-07-13 17:21:03 -0300

    ButtonBar Crianção

--- app/src/main/java/com/example/maintask/views/activity/MainActivity.kt
+++ app/src/main/java/com/example/maintask/views/activity/MainActivity.kt
@@ -11,63 +11,34 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.maintask.R
 import com.example.maintask.views.fragment.LoginFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var  toggle : ActionBarDrawerToggle
-    lateinit var drawerLayout: DrawerLayout
     private lateinit var navController: NavController
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        title = "Sua Agenda";
-
-
-        drawerLayout = findViewById(R.id.drawerLayout)
-        val navView : NavigationView = findViewById(R.id.nav_view)
-        toggle = ActionBarDrawerToggle(this, drawerLayout,R.string.open,R.string.close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.hide()
 
         val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.activity_main_navHostFragment) as NavHostFragment
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         // Define o navController a partir do navHost
         navController = navHostFragment.navController
 
-        navView.setNavigationItemSelectedListener {
-            it.isChecked = true
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        setupWithNavController(bottomNavigationView, navController)
 
-            when(it.itemId){
-                R.id.nav_profile -> replaceFragment(LoginFragment(), it.title.toString())
 
-            }
-            true
-        }
+
 
     }
 
-    private fun replaceFragment(fragment: Fragment, title: String){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout,fragment)
-        fragmentTransaction.commit()
-        drawerLayout.closeDrawers()
-        setTitle(title)
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        if(toggle.onOptionsItemSelected(item)){
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
 }
