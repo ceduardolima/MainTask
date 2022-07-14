028187f423deaa8ccf1280e3e33f5d8d3f94cb56
Eduardo <ceduardolima2000@gmail.com>
2022-07-14 14:42:36 -0300

    Criando abstract task

--- app/src/main/java/com/example/maintask/model/adapters/TaskAdapterAbstract.kt
+++ app/src/main/java/com/example/maintask/model/adapters/TaskAdapterAbstract.kt
@@ -1,24 +1,22 @@
 package com.example.maintask.model.adapters
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
-import androidx.core.view.size
-import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.example.maintask.R
 import com.example.maintask.model.TaskModel
 import java.time.LocalDate
 
-class TaskAdapter(context: Context, tasks: MutableList<TaskModel>) : TaskAdapterAbstract(context, tasks){
+abstract class TaskAdapterAbstract(
+    val context: Context,
+    val tasks: MutableList<TaskModel>) : RecyclerView.Adapter<TaskAdapterAbstract.TaskAbstractViewHolder>() {
 
     companion object {
         private val FORTODAY = 0
@@ -32,15 +30,6 @@ class TaskAdapter(context: Context, tasks: MutableList<TaskModel>) : TaskAdapter
         return TaskAbstractViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: TaskAbstractViewHolder, position: Int) {
-        holder.titleTextView.text = tasks[position].title
-        holder.dateTextView.text = daysLeft(tasks[position].date.dayOfYear - LocalDate.now().dayOfYear)
-        holder.descriptionTextView.text = tasks[position].description
-        holder.auhtorTextView.text = tasks[position].author
-
-        holder.itemContainer.setOnClickListener { view -> changeDetailsVisibility(view, holder.containerDetails) }
-    }
-
     override fun getItemCount() = tasks.size
 
     private fun daysLeft(days: Int) =
@@ -48,10 +37,9 @@ class TaskAdapter(context: Context, tasks: MutableList<TaskModel>) : TaskAdapter
             days == 0 -> "Hoje"
             days > 0 -> "Faltam ${days} dias"
             else -> "${days * (-1)} atrasado"
-    }
+        }
 
-
-    inner class TaskViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    inner class TaskAbstractViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val titleTextView = view.findViewById<TextView>(R.id.task_item_title)
         val dateTextView = view.findViewById<TextView>(R.id.task_item_date)
         val auhtorTextView = view.findViewById<TextView>(R.id.task_item_author)
@@ -74,6 +62,4 @@ class TaskAdapter(context: Context, tasks: MutableList<TaskModel>) : TaskAdapter
             params.height = 210
             view.layoutParams = params
         }
-
-
 }
