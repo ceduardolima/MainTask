003e58c0fe0beeaa57ac01e7014aac18d4309e47
Eduardo <ceduardolima2000@gmail.com>
2022-09-05 19:53:21 -0300

    refatorando detailsFragment

--- app/src/main/java/com/example/maintask/viewmodel/TimerCounterProcessViewModel.kt
+++ app/src/main/java/com/example/maintask/viewmodel/TimerCounterProcessViewModel.kt
@@ -1,4 +1,111 @@
 package com.example.maintask.viewmodel
 
-class TimerCounterProcessViewModel {
+import android.app.Application
+import android.util.Log
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.*
+import com.example.maintask.model.database.application.RoomApplication
+import com.example.maintask.model.database.entity.ActionEntity
+import com.example.maintask.model.database.entity.TaskEntity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class TimerCounterProcessViewModel(application: Application): AndroidViewModel(application) {
+    private var roomViewModel: RoomViewModel
+
+    private val _taskId = MutableLiveData<Int>()
+    val taskId: LiveData<Int>
+        get() = _taskId
+
+    private val _currentTask = MutableLiveData<TaskEntity>()
+    val currentTask: LiveData<TaskEntity>
+        get() = _currentTask
+
+    private var _actionList = MutableLiveData<List<ActionEntity>>()
+    val actionList: LiveData<List<ActionEntity>>
+        get() = _actionList
+
+    private var _actionTitleList = MutableLiveData<List<String>>()
+    val actionTitleList: LiveData<List<String>>
+        get() = _actionTitleList
+
+    private val _detailsFragmentButtonClick = MutableLiveData<Boolean>()
+    val detailsFragmentButtonClick: LiveData<Boolean>
+        get() = _detailsFragmentButtonClick
+
+    private val _progressBar = MutableLiveData<Boolean>()
+    val progressBar: LiveData<Boolean>
+        get() = _progressBar
+
+    init {
+        val roomApplication = (application as RoomApplication)
+        roomViewModel = RoomViewModelFactory(
+            roomApplication.taskRepository,
+            roomApplication.actionRepository,
+            roomApplication.taskActionRepository
+        ).create(RoomViewModel::class.java)
+    }
+
+    fun loadData(block: () -> Unit){
+        viewModelScope.launch {
+            _progressBar.value = false
+            block()
+            delay(500)
+            _progressBar.value = true
+        }
+    }
+
+    fun loadTaskAndActions(fragmentActivity: FragmentActivity, taskId: Int) {
+        if(taskId != -1) {
+            loadTask(fragmentActivity, taskId)
+            loadActionList(fragmentActivity, taskId)
+        }
+    }
+
+    private fun loadTask(fragmentActivity: FragmentActivity, taskId: Int) {
+        roomViewModel.getTaskById(taskId).observe(fragmentActivity, Observer {
+            task -> _currentTask.value = task
+        })
+    }
+
+    private fun loadActionList(fragmentActivity: FragmentActivity, taskId: Int) {
+        roomViewModel.getActionByTaskId(taskId)
+            .observe(fragmentActivity, Observer { actionList ->
+            this._actionList.value = actionList
+            setActionTitleList(actionList)
+        })
+    }
+
+    private fun setActionTitleList(actionList: List<ActionEntity>) {
+        val stringList = mutableListOf<String>()
+        actionList.forEach {
+                action -> stringList.add(action.action)
+        }
+        _actionTitleList.value = stringList
+    }
+
+    fun observeIfWasDataLoaded(fragmentActivity: FragmentActivity, block: () -> Unit) {
+        _progressBar.observe(fragmentActivity, Observer { wasLoaded ->
+              if(wasLoaded) {
+                  block()
+              }
+        })
+    }
+
+    fun observerDetailFragmentButtonClickAndNavigate(
+        fragmentActivity: FragmentActivity,
+        navigate: () -> Unit){
+        _detailsFragmentButtonClick.observe(fragmentActivity, Observer { click ->
+            if (click) {
+                navigate()
+                _detailsFragmentButtonClick.value = false
+                _detailsFragmentButtonClick.removeObservers(fragmentActivity)
+            }
+        })
+    }
+
+    fun setButtonClick(wasClicked: Boolean) {
+        _detailsFragmentButtonClick.value = wasClicked
+    }
+
 }
--- app/src/main/java/com/example/maintask/views/fragment/DetailTaskFragment.kt
+++ app/src/main/java/com/example/maintask/views/fragment/DetailTaskFragment.kt
@@ -10,22 +10,15 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.maintask.R
-import com.example.maintask.model.database.application.RoomApplication
 import com.example.maintask.model.database.entity.TaskEntity
-import com.example.maintask.viewmodel.DetailTaskViewModel
-import com.example.maintask.viewmodel.RoomViewModel
-import com.example.maintask.viewmodel.RoomViewModelFactory
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.launch
+import com.example.maintask.model.daysLeft.DaysLeft
+import com.example.maintask.viewmodel.TimerCounterProcessViewModel
 import java.time.LocalDate
-import kotlin.properties.Delegates
 
 class DetailTaskFragment : Fragment() {
-    private var taskId by Delegates.notNull<Int>()
     private lateinit var scrollView: NestedScrollView
     private lateinit var progressBar: ProgressBar
     private lateinit var taskTitle: TextView
@@ -35,38 +28,14 @@ class DetailTaskFragment : Fragment() {
     private lateinit var taskActions: TextView
     private lateinit var taskTools: TextView
     private lateinit var goToTimerFragmentButton: Button
-    private val detailTaskViewModel: DetailTaskViewModel by viewModels()
-    private val roomViewModel: RoomViewModel by viewModels {
-        val roomApplication = (requireActivity().application as RoomApplication)
-        RoomViewModelFactory(
-            roomApplication.taskRepository,
-            roomApplication.actionRepository,
-            roomApplication.taskActionRepository
-        )
-    }
+    private val timerCounterProcessViewModel: TimerCounterProcessViewModel by activityViewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        taskId = requireArguments().getInt("task_id")
-        detailTaskViewModel.loadData {
-            getCurrentTask()
-            getCurrentActionList()
-        }
-    }
-
-    private fun getCurrentTask() {
-        roomViewModel.allTasks.observe(requireActivity()) { taskList ->
-            detailTaskViewModel.findAndSetTask(taskId, taskList)
-        }
-    }
-
-    private fun getCurrentActionList() {
-        roomViewModel.getActionByTaskId(taskId).observe(requireActivity()) { actionList ->
-            val actionStringList = mutableListOf<String>()
-            detailTaskViewModel.setActionList(actionList)
-            for (action in actionList)
-                actionStringList.add(action.action)
-            detailTaskViewModel.setActionStringList(actionStringList)
+        val taskId = requireArguments().getInt("task_id")
+        timerCounterProcessViewModel.loadData {
+            timerCounterProcessViewModel.loadTaskAndActions(requireActivity(), taskId)
         }
     }
 
@@ -75,12 +44,14 @@ class DetailTaskFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_detail_task, container, false)
-        initializeVariables(view)
-        setupFragmentInformation()
+        initializeViews(view)
+        setInformationWhenDataLoad()
+        getButtonClick()
+        changeToTimerFragment()
         return view
     }
 
-    private fun initializeVariables(view: View) {
+    private fun initializeViews(view: View) {
         taskTitle = view.findViewById(R.id.details_task_title)
         deadline = view.findViewById(R.id.details_task_days)
         taskAuthor = view.findViewById(R.id.details_task_author)
@@ -92,31 +63,48 @@ class DetailTaskFragment : Fragment() {
         scrollView = view.findViewById(R.id.detail_fragment_scroll_view)
     }
 
-    private fun setupFragmentInformation() {
-        detailTaskViewModel.progressBar.observe(requireActivity()) { isLoadingData ->
-            if (!isLoadingData) {
-                setFragmentInformation()
-                changeToTimerFragment()
-                ableVisibility()
-            }
+    private fun setInformationWhenDataLoad() {
+        timerCounterProcessViewModel.observeIfWasDataLoaded(requireActivity()) {
+            setFragmentInformation()
+            ableVisibility()
         }
     }
 
     private fun setFragmentInformation() {
-        detailTaskViewModel.currentTask.observe(requireActivity()) { task ->
-            detailTaskViewModel.actionStringList.observe(requireActivity()) { stringList ->
-                setAllText(task, stringList.joinToString(", "))
+        val actionTitleList = timerCounterProcessViewModel.actionTitleList
+        val currentTask = timerCounterProcessViewModel.currentTask
+        currentTask.observe(requireActivity()) { task ->
+            if(task != null) {
+                actionTitleList.observe(requireActivity()) { titleList ->
+                    setAllText(task, titleList.joinToString(", "))
+                }
             }
         }
     }
 
-    private fun changeToTimerFragment() {
+    @SuppressLint("SetTextI18n")
+    private fun setAllText(currentTask: TaskEntity, actionsString: String) {
+        taskTitle.text = currentTask.title
+        deadline.text = DaysLeft
+            .getDaysLeftString(LocalDate.parse(currentTask.date).dayOfYear)
+        taskAuthor.text = "Autor: ${currentTask.author}"
+        taskDescription.text = "Descrição: ${currentTask.description}"
+        taskActions.text = "Ações: $actionsString"
+        taskTools.text = "Ferramentas: ${currentTask.tools}"
+    }
+
+    private fun getButtonClick() {
         goToTimerFragmentButton.setOnClickListener {
-            val extras = Bundle()
-            extras.putInt("task_id", taskId)
-            findNavController()
-                .navigate(R.id.action_detailTaskFragment_to_timerFragment, extras)
+            timerCounterProcessViewModel.setButtonClick(true)
         }
+    }
+
+    private fun changeToTimerFragment() {
+        timerCounterProcessViewModel
+            .observerDetailFragmentButtonClickAndNavigate(requireActivity()) {
+                findNavController()
+                    .navigate(R.id.action_detailTaskFragment_to_timerFragment)
+            }
     }
 
     private fun ableVisibility() {
@@ -124,43 +112,18 @@ class DetailTaskFragment : Fragment() {
         scrollView.visibility = View.VISIBLE
     }
 
-    private fun disableVisibility() {
-        progressBar.visibility = View.VISIBLE
-        scrollView.visibility = View.INVISIBLE
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun setAllText(currentTask: TaskEntity, actionsString: String) {
-        taskTitle.text = currentTask.title
-        deadline.text = daysLeft(LocalDate.parse(currentTask.date).dayOfYear)
-        taskAuthor.text = "Autor: ${currentTask.author}"
-        taskDescription.text = "Descrição: ${currentTask.description}"
-        taskActions.text = "Ações: $actionsString"
-        taskTools.text = "Ferramentas: ${currentTask.tools}"
-    }
-
-    private fun daysLeft(days: Int): String {
-        val today = LocalDate.now().dayOfYear
-        val daysLeft = days - today
-
-        return when {
-            daysLeft == 0 -> "Para hoje"
-            daysLeft > 0 -> "Faltam ${daysLeft} dias"
-            else -> "Esta à ${daysLeft * (-1)} dia(s) atrasado"
-        }
-    }
-
     override fun onDestroyView() {
         disableVisibility()
         super.onDestroyView()
     }
 
+    private fun disableVisibility() {
+        progressBar.visibility = View.VISIBLE
+        scrollView.visibility = View.INVISIBLE
+    }
+
     fun getBackAndResetActionTimer() {
-        val actionList = detailTaskViewModel.actionList
         val navController = findNavController()
-        CoroutineScope(Main).launch {
-            roomViewModel.resetElapsedTime(actionList)
-            navController.navigate(R.id.action_detailTaskFragment_to_taskFragment)
-        }
+        navController.navigate(R.id.action_detailTaskFragment_to_taskFragment)
     }
 }
